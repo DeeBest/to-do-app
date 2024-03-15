0cb976fd780265adc3cc2c97f406a9b6fde399b8
DeeBest <simphiwedladla8@gmail.com>
2024-03-15 09:40:20 +0200

    created the delete function in the create project class

--- src/functionality/createProject.js
+++ src/functionality/createProject.js
@@ -1,17 +1,23 @@
-import { projects, newProject, populateProjects } from "./populateProjects";
+// Import populateProjects function from populateProjects.js
+import { populateProjects } from "./populateProjects";
 
-export function createProject(){
+// Array to store projects
+export let projects = [];
 
+// Function to create a new project
+export function createProject() {
+  // Define Project class
   class Project {
-    constructor(projectName) {
-      this.projectName = projectName;
+    constructor(name) {
+      this.name = name;
       this.todos = [];
+      this.notes = [];
     }
-  
+
     addTodo(todo) {
       this.todos.push(todo);
     }
-  
+
     editTodo(todoIndex, updatedTodo) {
       if (todoIndex >= 0 && todoIndex < this.todos.length) {
         this.todos[todoIndex] = updatedTodo;
@@ -19,42 +25,41 @@ export function createProject(){
         console.error("Invalid todo index");
       }
     }
-  
+
+    addNote(note) {
+      this.notes.push(note);
+    }
+
     deleteProject() {
-      // Implementation of deleting the project would depend on your application structure.
-      // You may want to remove the project from a list of projects or perform any cleanup tasks.
-      // For now, let's just log a message.
-      console.log(`Project '${this.projectName}' deleted.`);
+      const index = projects.indexOf(this); // Find the index of the current project in the projects array
+      if (index !== -1) {
+        projects.splice(index, 1); // Remove the project object from the projects array
+        console.log(`Project '${this.name}' deleted.`);
+      } else {
+        console.error("Project not found in array");
+      }
     }
   }
 
-  //creates new project based on the Project class
-  let myProject = new Project('Workout');
-  
-  //creates a new to-do for the project
-  myProject.addTodo('Do Chest');
+  // Create a new project
+  let myProject = new Project("Workout");
 
-  //console logs the created project and its methods and its to-dos
-  console.log(myProject)
+  // Add a new todo for the project
+  myProject.addTodo("Do Chest");
 
-  //console logs the created project name
-  console.log(myProject.projectName);
-
-  //console logs the to-dos in a project
+  // Log the created project and its properties
+  console.log(myProject);
+  console.log(myProject.name);
   console.log(myProject.todos);
 
-  //stores the project name in the empty newProject variable
-  //so that it can be used to display the project name in the projects grid item
-  newProject = myProject.projectName;
+  // Add the newly created project to the projects array
+  projects.push(myProject);
 
-  //adds the newly created project in the projects array 
-  projects.push(newProject);
-
-  //console logs the projects that are stored in the projects array
+  // Log the projects stored in the projects array
+  console.log(projects);
+  myProject.deleteProject();
   console.log(projects);
 
-
-
-
+  // Call the populateProjects function to display the projects in the UI
   populateProjects();
-};
+}
--- src/functionality/populateProjects.js
+++ src/functionality/populateProjects.js
@@ -1,51 +1,61 @@
 import { projectsDiv } from "../components/main/main.js";
-import './populateProjects.css'
+import "./populateProjects.css";
 
-//an empty array to store the projects
-export let projects = []; 
-export let newProject;
+// Import projects array from createProject.js
+import { projects } from "./createProject";
 
-//function to display the projects in the projects grid item
+// Function to populate the projects grid item
 export function populateProjects() {
-  let projectsList = document.createElement('div');
-  projectsList.id = 'project-list';
+  // Create a container for the projects list
+  let projectsList = document.createElement("div");
+  projectsList.id = "project-list";
 
-  for (let i = 0; i < projects.length; i++){
-    const projectItemDiv = document.createElement('div');
-    projectItemDiv.classList.add('project-item-div');
+  // Loop through the projects array
+  projects.forEach((project) => {
+    // Create a div for each project
+    const projectItemDiv = document.createElement("div");
+    projectItemDiv.classList.add("project-item-div");
 
-    let projectName = document.createElement('span');
-    projectName.classList.add('project-name');
-    projectName.textContent = projects[i];
+    // Create a span element to display the project name
+    let projectName = document.createElement("span");
+    projectName.classList.add("project-name");
+    projectName.textContent = project.name;
 
-    let projectItemBtnsDiv = document.createElement('div');
-    projectItemBtnsDiv.classList.add('project-items-btns-div');
+    // Create div for project item buttons
+    let projectItemBtnsDiv = document.createElement("div");
+    projectItemBtnsDiv.classList.add("project-items-btns-div");
 
-    let projectAddToDoBtn = document.createElement('button');
-    projectAddToDoBtn.classList.add('project-item-button');
-    projectAddToDoBtn.id = 'project-add-to-do-button';
+    // Create button to add a todo
+    let projectAddToDoBtn = document.createElement("button");
+    projectAddToDoBtn.classList.add("project-item-button");
+    projectAddToDoBtn.id = "project-add-to-do-button";
     projectAddToDoBtn.textContent = `+ To Do`;
 
-    let projectEditBtn = document.createElement('button');
-    projectEditBtn.classList.add('project-item-button');
-    projectEditBtn.id = 'project-edit-button';
-    projectEditBtn.textContent = 'Edit';
+    // Create button to edit the project
+    let projectEditBtn = document.createElement("button");
+    projectEditBtn.classList.add("project-item-button");
+    projectEditBtn.id = "project-edit-button";
+    projectEditBtn.textContent = "Edit";
 
-    let projectDeleteBtn = document.createElement('button');
-    projectDeleteBtn.classList.add('project-item-button');
-    projectDeleteBtn.id = 'project-delete-button';
-    projectDeleteBtn.textContent = 'Delete';
+    // Create button to delete the project
+    let projectDeleteBtn = document.createElement("button");
+    projectDeleteBtn.classList.add("project-item-button");
+    projectDeleteBtn.id = "project-delete-button";
+    projectDeleteBtn.textContent = "Delete";
 
-    projectItemBtnsDiv.appendChild(projectAddToDoBtn)
+    // Append buttons to the project item buttons div
+    projectItemBtnsDiv.appendChild(projectAddToDoBtn);
     projectItemBtnsDiv.appendChild(projectEditBtn);
     projectItemBtnsDiv.appendChild(projectDeleteBtn);
 
+    // Append project name and buttons div to the project item div
     projectItemDiv.appendChild(projectName);
     projectItemDiv.appendChild(projectItemBtnsDiv);
 
+    // Append project item div to the projects list container
     projectsList.appendChild(projectItemDiv);
-  }
+  });
 
+  // Append the projects list container to the projects div in the DOM
   projectsDiv.appendChild(projectsList);
-};
- 
+}
