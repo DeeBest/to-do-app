b0a043d2956a6bf6196edbb8058c3ff85b4f1cb2
DeeBest <simphiwedladla8@gmail.com>
2024-03-29 09:30:14 +0200

    created a projectBody div that will have a notes sectiction

--- src/functionality/createProject.js
+++ src/functionality/createProject.js
@@ -15,23 +15,48 @@ export function createProject() {
     projectItem.classList.add('projectItem');
     projectsContainer.appendChild(projectItem);
 
+    let projectHeader = document.createElement('div');
+    projectHeader.classList.add('projectHeader');
+    projectItem.appendChild(projectHeader);
+
+    let projectCheckBox = document.createElement('input');
+    projectCheckBox.classList.add('projectCheckBox');
+    projectCheckBox.type = 'checkbox';
+    projectHeader.appendChild(projectCheckBox);
+
     let projectName = document.createElement('span');
     projectName.classList.add('projectName');
     projectName.textContent = newProject.name;
-    projectItem.appendChild(projectName);
+    projectHeader.appendChild(projectName);
 
-    let addToDoBtn = document.createElement('button');
-    addToDoBtn.classList.add('addToDoBtn');
-    addToDoBtn.textContent = '+To-Do';
-    projectItem.appendChild(addToDoBtn);
+    let dueDate = document.createElement('span');
+    dueDate.classList.add('dueDate');
+    dueDate.textContent = '20/06/2024';
+    projectHeader.appendChild(dueDate);
 
-    let removeProjectBtn = document.createElement('button');
-    removeProjectBtn.classList.add('removeProjectBtn');
-    removeProjectBtn.textContent = '-Project';
-    projectItem.appendChild(removeProjectBtn);
+    let arrowSpan = document.createElement('span');
+    arrowSpan.classList.add('arrowSpan');
+    arrowSpan.textContent = '▼';
+    projectHeader.appendChild(arrowSpan);
+
+    let projectBody = document.createElement('div');
+    projectBody.classList.add('projectBody');
+    projectBody.textContent = 'project body';
+    projectItem.appendChild(projectBody);
+    
+    // let addToDoBtn = document.createElement('button');
+    // addToDoBtn.classList.add('addToDoBtn');
+    // addToDoBtn.textContent = '+To-Do';
+    // projectItem.appendChild(addToDoBtn);
+
+    // let removeProjectBtn = document.createElement('button');
+    // removeProjectBtn.classList.add('removeProjectBtn');
+    // removeProjectBtn.textContent = '-Project';
+    // projectItem.appendChild(removeProjectBtn);
 
     projectNameInput.value = '';
   } else {
     alert('Please enter a project name!');
   }
 }
+
